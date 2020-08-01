4ddaa7b8179c6f6881a7cdcd27d01632e8af7d08
nokin-all-of-career <allofcareerapp@gmail.com>
2020-08-01 18:24:59 +0900

    implement callbacks

--- tutorial001/src/App.js
+++ tutorial001/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect,useState} from 'react';
 
 
 const App = props => {
@@ -10,12 +10,25 @@ const App = props => {
   const [state,setState] = useState(props)
   const {name,price} = state
 
+  // renderringの後に呼ばれる
+  useEffect(() =>{
+    console.log('use useEffect')
+  })
+
+  useEffect(() =>{
+    console.log('this is like componentDidMount')
+  },[])
+
+  useEffect(() =>{
+    console.log('this callback is for name only')
+  },[name])
+
   const reset = () => setState(props)
 
 
   return (
     <React.Fragment>
-      <p>現在の{name}、現在の{price}</p>
+      <p>現在の{name}、現在の{price}です</p>
       <button onClick={()=>setState({...state,price:state.price+1})}>+1</button>
       <button onClick={()=>setState({...state,price:state.price-1})}>-1</button>
       <button onClick={reset}>Reset</button>
