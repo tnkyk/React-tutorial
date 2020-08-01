ffce03e49081e2ecc3faff61bba756ec69765aa6
nokin-all-of-career <allofcareerapp@gmail.com>
2020-08-01 16:42:24 +0900

    my first React Hook with useState

--- tutorial001/src/App.js
+++ tutorial001/src/App.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const App = () => {
+  //初期値を与えることができる
+  //配列を返す　[変数、変数を加工できる関数]
+  const [count,setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+
+  const reset = () => setCount(0)
+
+  const x2 = () => setCount(count * 2)
+
+  const dic3 = () => setCount(previousCount => {
+    if(previousCount % 3 === 0){
+      return previousCount / 3
+    }else{
+      return previousCount
+    }
+  })
+
   return (
-    <div>
-      This is a template for React app
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={x2}>x2</button>
+        <button onClick={dic3}>3の倍数の時だけ３で割る</button>
+      </div>
+    </React.Fragment>
   );
 }
 
