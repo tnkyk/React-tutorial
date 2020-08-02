5aa4cd0d48afbd4e4838bc6c1951328a1da6434b
nokin-all-of-career <allofcareerapp@gmail.com>
2020-08-02 15:27:59 +0900

    create reducer for events

--- tutorial001/src/components/App.js
+++ tutorial001/src/components/App.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React,{ useState,useReducer } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+import reducer from '../reducers/index'
+
 const App = () => {
+  //reducerの使い方,dispatchは状態遷移を行う時に呼ばれる
+  const [state,dispatch] = useReducer(reducer,[])
+  const [title,setTitle] = useState('')
+  const [body,setBody] = useState('')
+
+  const addEvent = e => {
+    e.preventDefault()
+    console.log({state})
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body,
+    })
+
+    setTitle('')
+    setBody('')
+  }
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
       <form>
         <div className="form-group">
           <label htmlFor="formEventTitle">タイトル</label>
-          <input className="form-control" id="formEventTitle"></input>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e => setTitle(e.target.value)}></input>
         </div>
         <div className="form-group">
-          <label htmlFor="formEventBody">タイトル</label>
-          <textarea className="form-control" id="formEventBody"></textarea>
+          <label htmlFor="formEventBody">ボディー</label>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e => setBody(e.target.value)}></textarea>
         </div>
 
-        <button className="btn btn-primary">イベントを作成</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成</button>
         <button className="btn btn-danger">全てイベントを削除する</button>
       </form>
 
---
+++ tutorial001/src/reducers/index.js
@@ -0,0 +1,31 @@
+// action = {
+//     type: 'CREATE_EVENT',
+//     title: '',
+//     body: ''
+// }
+
+// state = []
+// state = [
+//     {id:1,title:'',body:''},
+//     {id:1,title:'',body:''},
+//     {id:1,title:'',body:''},
+// ]
+
+
+const events = (state = [],action) => {
+    switch(action.type){
+        case 'CREATE_EVENT':
+            const event = {title: action.title, body: action.body}
+            const length = state.length
+            const id = length === 0 ? 1 : state[length - 1].id + 1
+            return [...state,{id,...event}]
+        case 'DELETE_EVENT':
+            return state
+        case 'DELETE_ALL_EVENTS':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
