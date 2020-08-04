fe48d5b74c6bfe9d336ec51a97ef849d740dd816
nokin-all-of-career <allofcareerapp@gmail.com>
2020-08-04 14:19:25 +0900

    setup react context provider

--- tutorial001/src/components/App.js
+++ tutorial001/src/components/App.js
@@ -7,15 +7,20 @@ import Events from './Events'
 import EventForm from './EventForm'
 import reducer from '../reducers/index'
 
+import AppContext from '../contexts/AppContext'
+
 const App = () => {
   //reducerの使い方,dispatchは状態遷移を行う時に呼ばれる
   const [state,dispatch] = useReducer(reducer,[])
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-    </div>
+    <AppContext.Provider value={"Hello.I'm provider"}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}/>
+        <Events state={state} dispatch={dispatch}/>
+      </div>
+    </AppContext.Provider>
+
   );
 }
 
