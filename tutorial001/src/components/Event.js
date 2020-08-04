4d247bbc984dac50fd3de52a8bcff61af9091f99
nokin-all-of-career <allofcareerapp@gmail.com>
2020-08-04 13:59:05 +0900

    replace type names with constants

--- tutorial001/src/components/Event.js
+++ tutorial001/src/components/Event.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import {DELETE_EVENT} from '../actions'
+
 const Event = ({event,dispatch}) => {
     const id = event.id
     const handleClickDeleteButton = () => {
         const result  = window.confirm(`(id=${id})イベントを削除しても良いですか？`)
-        if (result) dispatch({type:'DELETE_EVENT',id})
+        if (result) dispatch({type:DELETE_EVENT,id})
     }
     return (
         <tr>
--- tutorial001/src/components/EventForm.js
+++ tutorial001/src/components/EventForm.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 
+import {DELETE_ALL_EVENT,CREATE_EVENT} from '../actions'
 
 const EventForm = ({state,dispatch}) => {
   const [title,setTitle] = useState('')
@@ -9,7 +10,7 @@ const EventForm = ({state,dispatch}) => {
   const addEvent = e => {
     e.preventDefault()
     dispatch({
-      type: 'CREATE_EVENT',
+      type: CREATE_EVENT,
       title,
       body,
     })
@@ -21,7 +22,7 @@ const EventForm = ({state,dispatch}) => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result  = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({type:'DELETE_ALL_EVENTS'})
+    if (result) dispatch({type:DELETE_ALL_EVENT})
   }
     return (
         <>
